1eb5ffd3bbe1bf47f74cfae30c56bb9762775bb2
superman <superman@163.com>
2016-07-08 15:43:55 +0800

    修改了更多内容

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+
+window.onload=function(){
+     init();
+     function init(){
+     	addEvent();
+     }
+     function addEvent(){
+     	$("#more").on("click",function(){
+     		var text=$(this).text();
+     		if(text=="查看更多"){
+     			$("#more").parent().css({
+	     			"height":"auto",
+	     			"transition":"all 3s"
+     			})
+     			$("#more").text("收到碗里");
+     		}else if(text=="收到碗里"){
+     			$("#more").parent().css({
+	     			"height":"300px",
+	     			"transition":"all 3s"
+     			})
+     			$("#more").text("查看更多");
+     		}
+     		
+     	})
+     }
+}
